1123f35b0c66033bf05dc624beda680971d71995
Devman <leonardopachecoduran@gmail.com>
2020-07-14 23:06:32 -0400

    crud
    a simple MVC project for database connection, WIP

---
+++ CRUDotnet/CRUDotnet/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using prodBLL;
+
+namespace CRUDotnet
+{
+    public partial class Form1 : Form
+    {
+        CN_Productos objetoCN = new CN_Productos();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MuestraProductos();
+        }
+
+        public void MuestraProductos()
+        {
+            dataGridView1.DataSource = objetoCN.MostrarProd();
+        }
+    }
+}

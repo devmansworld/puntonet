1fa5c52ed6c743f480f98cc61eb5d53113c05471
Devman <leonardopachecoduran@gmail.com>
2020-07-14 23:02:52 -0400

    a simple calculator

---
+++ calculadora/calculadora/Model/numeros.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora.Model
+{
+    public class numeros
+    {
+        #region Atributos
+        private decimal _n1;
+        private decimal _n2;
+        #endregion
+        #region Constructor
+        public numeros()
+        {
+            N1 = 0;
+            N2 = 0;
+        }
+
+        public numeros(decimal N1, decimal N2)
+        {
+            this.N1 = N1;
+            this.N2 = N2;
+        }
+
+        #endregion
+        #region Propiedades
+        public decimal N1 { get => _n1; set => _n1 = value; }
+        public decimal N2 { get => _n2; set => _n2 = value; }
+        #endregion
+        #region Metodos
+
+        public string obtenerDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Primer número  : ").Append(_n1).Append(Environment.NewLine);
+            sb.Append("Segundo número : ").Append(_n2).Append(Environment.NewLine);
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ calculadora/calculadora/View/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using calculadora.Controller;
+using calculadora.Model;
+
+namespace calculadora
+{
+    public partial class F_calculadora : Form
+    {
+        public F_calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void b_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void b_limpiar_Click(object sender, EventArgs e)
+        {
+            tb_n1.Text = "";
+            tb_n2.Text = "";
+            rb_suma.Checked = false;
+            rb_resta.Checked = false;
+            rb_multiplicacion.Checked = false;
+            rb_division.Checked = false;
+            l_resultado.Text = "Resultado:";
+        }
+
+        private void b_calcular_Click(object sender, EventArgs e)
+        {
+            //recibo datos
+            string num1 = tb_n1.Text;
+            string num2 = tb_n2.Text;
+            bool su = rb_suma.Checked;
+            bool re = rb_resta.Checked;
+            bool mu = rb_multiplicacion.Checked;
+            bool di = rb_division.Checked;
+            validacion v = new validacion();
+            if (v.cVacia(num1)==false || v.cVacia(num2)==false)
+            {
+                MessageBox.Show("Falta ingresar, al menos, un número");
+            }
+            else
+            {
+                if (v.vNumero(num1) == false || v.vNumero(num2) == false)
+                {
+                    MessageBox.Show("Un valor ingresado no es un número");
+                }
+                else
+                {
+                    bool re1 = re;
+                    if (su == false && re1 == false && mu == false && di == false)
+                    {
+                        MessageBox.Show("No se ha seleccionado la operación");
+                    }
+                    else
+                    {
+                        //aplican metodos
+                        controlador c = new controlador();
+                        numeros n = new numeros();
+                        n.N1 = Convert.ToDecimal(num1);
+                        n.N2 = Convert.ToDecimal(num2);
+                        decimal r;
+                        int oper;
+                        if (su == true)
+                        {
+                            r = c.suma(n.N1, n.N2);
+                            oper = 1;
+                        }
+                        else if (re == true)
+                        {
+                            r = c.resta(n.N1, n.N2);
+                            oper = 2;
+                        }
+                        else if (mu == true)
+                        {
+                            r = c.multiplicacion(n.N1, n.N2);
+                            oper = 3;
+                        }
+                        else if (di == true)
+                        {
+                            if (di == true && n.N2 == 0)
+                            {
+                                r = 0;
+                                oper = 5;
+                            }
+                            else
+                            {
+                                r = c.division(n.N1, n.N2);
+                                oper = 4;
+                            }
+                            
+                        }
+                        else
+                        {
+                            r = 0;
+                            oper = 5;
+                        }
+                        if (di == true && n.N2 == 0)
+                        {
+                            l_resultado.Text = "Error matemático";
+                        }
+                        else
+                        {
+                            l_resultado.Text = "Resultado: \n" + c.formato(oper,r, n.N1, n.N2);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
